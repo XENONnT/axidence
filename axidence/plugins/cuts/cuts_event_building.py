62fcab19bd0cad216fb0bd4d16a2b114b3d6e6e4
Dacheng Xu <dx2227@columbia.edu>
2024-05-13 06:02:44 -0400

    Fix bug when triggering peak in an event is not from the salting peaks (#74)
    * Fix bug when triggering peak in an event is not from the salting peaks
    
    * Bump plugin version

--- axidence/plugins/cuts/cuts_event_building.py
+++ axidence/plugins/cuts/cuts_event_building.py
@@ -2,26 +2,26 @@ from strax import CutPlugin, CutList
 
 
 class MainS1Trigger(CutPlugin):
-    __version__ = "0.0.0"
+    __version__ = "0.1.0"
     depends_on = "event_basics"
     provides = "cut_main_s1_trigger"
     cut_name = "cut_main_s1_trigger"
     cut_description = "Whether the salting S1 can be the main S2"
 
     def cut_by(self, events):
-        mask = events["s1_salt_number"] >= 0
+        mask = events["s1_salt_number"] == events["salt_number"]
         return mask
 
 
 class MainS2Trigger(CutPlugin):
-    __version__ = "0.0.0"
+    __version__ = "0.1.0"
     depends_on = "event_basics"
     provides = "cut_main_s2_trigger"
     cut_name = "cut_main_s2_trigger"
     cut_description = "Whether the salting S2 can be the main S2"
 
     def cut_by(self, events):
-        mask = events["s2_salt_number"] >= 0
+        mask = events["s2_salt_number"] == events["salt_number"]
         return mask
 
 
